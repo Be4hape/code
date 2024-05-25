477ad7a9fce1bdf7f8e22b70d16b79baf53975e4
시형 <100893354+Be4hape@users.noreply.github.com>
2024-05-25 13:13:49 +0900

    Update testzz.0.0.1

--- python/2024/testzz.py
+++ python/2024/testzz.py
@@ -1,23 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 이미지 로드 및 데이터 타입 변환
-im = plt.imread("lena_gray.bmp")
-if im.ndim == 3:  # 이미지가 컬러라면 그레이스케일로 변환
-    im = np.dot(im[..., :3], [0.2989, 0.5870, 0.1140])
-im = im.astype(np.uint8)
+# 이미지 로드 및 그레이스케일로 변환
+im = plt.imread("lena_gray.bmp").astype(np.uint8)
 
 # 원본 이미지의 히스토그램 계산
 im_hist, im_bins = np.histogram(im.flatten(), bins=256, range=[0, 256])
 
-# 히스토그램 평활화 수행
-cdf = im_hist.cumsum()  # 누적 분포 함수 계산
-cdf_normalized = cdf * 255 / cdf[-1]  # CDF 정규화
-equalized_im = np.interp(im.flatten(), im_bins[:-1], cdf_normalized)  # 평활화된 값으로 매핑
-equalized_im = equalized_im.reshape(im.shape).astype(np.uint8)
+# 누적 분포 함수(CDF) 계산
+cdf = np.zeros(256)
+csum = 0
+ttl_pixels = im.size
 
-# 평활화된 이미지의 히스토그램 계산
-eq_hist, eq_bins = np.histogram(equalized_im.flatten(), bins=256, range=[0, 256])
+for i in range(256):
+    csum += im_hist[i]
+    cdf[i] = csum
+
+cdf_norm = cdf * 255 / ttl_pixels
+
+# 새로운 이미지 생성 및 맵핑
+equal_im = np.interp(im.flatten(), np.arange(256), cdf_norm).reshape(im.shape).astype(np.uint8)
+
+# 균등화된 이미지의 히스토그램 계산
+nim_hist, nim_bins = np.histogram(equal_im.flatten(), bins=256, range=[0, 256])
 
 # 결과 이미지와 히스토그램을 하나의 Figure에 출력
 plt.figure(figsize=(14, 7))
@@ -34,12 +39,12 @@ plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
 
 plt.subplot(223)
-plt.imshow(equalized_im, cmap='gray')
+plt.imshow(equal_im, cmap='gray')
 plt.title('Equalized Image')
 plt.axis('off')
 
 plt.subplot(224)
-plt.plot(eq_bins[:-1], eq_hist, color='black')
+plt.plot(nim_bins[:-1], nim_hist, color='black')
 plt.title('Histogram of Equalized Image')
 plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
