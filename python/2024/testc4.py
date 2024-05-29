3c97748d4dcf8b410811daddc9ec576557a88604
시형 <100893354+Be4hape@users.noreply.github.com>
2024-05-29 09:10:33 +0900

    Update testc4.py

--- python/2024/testc4.py
+++ python/2024/testc4.py
@@ -4,30 +4,14 @@ import matplotlib.pyplot as plt
 #이미지 리딩 및 im의 histogram 생성
 im = plt.imread("lena_gray.bmp").astype(np.uint8)
 im_hist, im_bins = np.histogram(im.flatten(), bins=256, range=(0,256))
+imf = im.astype(np.float32)
 
-
-#256빈공간 생성, 값을 누적시킬 csum, for 256까지 cdf에 더한 값 대입
-cdf = np.zeros(256)
-csum = 0
-ttl_pixels = im.size
-
-for i in range(256):
-    csum += im_hist[i]
-    cdf[i] = csum
-
- #In = (I - Min) * (Maxn - Minn) / (Max-Min) + Minn, Min = 0, Max = 255, 노멀라이징 연산
-cdf_norm = cdf * 255 / ttl_pixels
-
-
-#빈 im크기의 norm_im 생성, cdf에 누적한 값 cdf_norm값을 norm_im에 대입
-norm_im = np.zeros_like(im)
-for i in range(256):
-    #boolean indexing, im==i 가 True일 때, cdf_norm값을 norm_im에 대입
-    norm_im[im == i] = cdf_norm[i]
-
+##In = (I - Min) * (Maxn - Minn) / (Max-Min) + Minn, Min = 0, Max = 255, 노멀라이징 연산
+#((img_f -img_f.min()) * (255) / (img_f.max() - img_f.min())).astype(np.uint8)
+cdf_norm = (imf - imf.min()) * 255 / (imf.max() - imf.min())
 
 #norm_im의 histogram 생성
-nim_hist, nim_bins = np.histogram(norm_im.flatten(), bins = 256, range = (0,256))
+nim_hist, nim_bins = np.histogram(cdf_norm.flatten(), bins = 256, range = (0,256))
 
 plt.subplot(221)
 plt.imshow(im, 'gray')
@@ -36,7 +20,8 @@ plt.subplot(222)
 plt.plot(im_bins[:-1], im_hist)
 
 plt.subplot(223)
-plt.imshow(norm_im, 'gray')
+# plt.imshow(norm_im, 'gray')
+plt.imshow(cdf_norm, 'gray')
 
 plt.subplot(224)
 plt.plot(nim_bins[:-1], nim_hist)
